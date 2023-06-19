ed72328bdb0b95516112243fb137b03febe80429
AndreMatioski02 <matioskiandre4@gmail.com>
2023-06-18 21:31:22 -0300

    Exercício Cap. 4

---
+++ CP-4.js
@@ -0,0 +1,41 @@
+// A soma de um intervalo
+
+// function range(start, end, increment) {
+//     let result = [];
+//     for(let i = start; i <= end; i += increment) {
+//         result.push(i)
+//     }
+//     console.log(result);
+//     let sum = 0;
+//     result.map(index => {
+//         sum += index
+//     })
+//     console.log(sum);
+// }
+
+// range(0, 10, 2);
+
+// Invertendo um array
+
+// function reverse(array) {
+//     let reversedArray = []
+//     for(let i = array.length; i > 0; i--) {
+//         reversedArray.push(array[i - 1])
+//     }
+//     console.log(reversedArray);
+// }
+
+// reverse(['A', 'B', 'C']);
+
+// function reverse(array) {
+//     for(let i = array.length; i > 0; i--) {
+//         array.push(array[i - 1])
+//     }
+
+//     for(let i = 0; i <= array.length / 2; i++) {
+//         array.shift();
+//     }
+//     console.log(array);
+// }
+
+// reverse(['A', 'B', 'C']);
