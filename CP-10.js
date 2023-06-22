0b865fca541e525c57d23f9bfd0fffccce885d68
AndreMatioski02 <matioskiandre4@gmail.com>
2023-06-22 20:55:47 -0300

    Exercício cap. 10

---
+++ CP-10.js
@@ -0,0 +1,13 @@
+const findTheMounth = function () {
+  const mounths = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+  return {
+    name: function (number) { return mounths[number]; },
+    number: function (name) { return mounths.indexOf(name); }
+  };
+}();
+
+
+console.log(findTheMounth.name(2));
+
+console.log(findTheMounth.number("Novembro"));
